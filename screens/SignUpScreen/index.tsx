dda1263efa833ab207b1c7f6fd30b6da06804d87
Nic Chappell <nwc@nicchappell.com>
2021-02-01 20:48:54 -0700

    add firebase (#3)

--- screens/SignUpScreen/index.tsx
+++ screens/SignUpScreen/index.tsx
@@ -1,32 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { ISignUpScreen } from './interfaces';
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import { SafeAreaView } from "react-native-safe-area-context";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { ISignUpScreen } from "./interfaces";
 
 const SignUpScreen = ({ navigation, route }: ISignUpScreen) => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text>This is top text.</Text>
-            <View>
-                <Text>Join Summit Club (SignUpScreen)</Text>
-                <Button
-                    title="Go back to Sign In"
-                    onPress={() => navigation.goBack()}
-                />
-            </View>
-            <Text>This is bottom text.</Text>
-        </SafeAreaView>
-    );
+  // effect hooks
+  useEffect(() => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword("test@test.test", "test1234")
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(error);
+      });
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>This is top text.</Text>
+      <View>
+        <Text>Join Summit Club (SignUpScreen)</Text>
+        <Button
+          title="Go back to Sign In"
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+      <Text>This is bottom text.</Text>
+    </SafeAreaView>
+  );
 };
 
 export default SignUpScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        flex: 1,
-        justifyContent: 'space-between'
-    },
+  container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "space-between",
+  },
 });

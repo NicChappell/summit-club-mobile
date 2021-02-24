a0f01fef26a20353d75697b9c4b23aeb16766494
NicChappell <nwc@nicchappell.com>
2021-02-24 06:59:46 -0700

    add feature photos

--- screens/HomeScreen/images/features/index.ts
+++ screens/HomeScreen/images/features/index.ts
@@ -5,89 +5,115 @@ export const CHALLENGER_POINT = require("./challenger-point.jpg");
 export const CONUNDRUM_PEAK = require("./conundrum-peak.jpg");
 export const CRESTONE_NEEDLE = require("./crestone-needle.jpg");
 export const CRESTONE_PEAK = require("./crestone-peak.jpg");
+export const CULEBRA_PEAK = require("./culebra-peak.jpg");
 export const EL_DIENTE_PEAK = require("./el-diente-peak.jpg");
+export const ELLINGWOOD_POINT = require("./ellingwood-point.jpg");
 export const GRAYS_PEAK = require("./grays-peak.jpg");
-export const HUMBOLT_PEAK = require("./humbolt-peak.jpg");
+export const HANDIES_PEAK = require("./handles-peak.jpg");
+export const HUMBOLDT_PEAK = require("./humbolt-peak.jpg");
+export const HURON_PEAK = require("./huron-peak.jpg");
 export const KIT_CARSON_PEAK = require("./kit-carson-peak.jpg");
 export const LA_PLATA_PEAK = require("./la-plata-peak.jpg");
+export const LITTLE_BEAR_PEAK = require("./little-bear-peak.jpg");
 export const LONGS_PEAK = require("./longs-peak.jpg");
 export const MAROON_PEAK = require("./maroon-peak.jpg");
 export const MISSOURI_MOUNTAIN = require("./missouri-mountain.jpg");
 export const MT_ANTERO = require("./mt-antero.jpg");
 export const MT_BELFORD = require("./mt-belford.jpg");
 export const MT_BIERSTADT = require("./mt-bierstadt.jpg");
+export const MT_BROSS = require("./mt-bross.jpg");
+export const MT_CAMERON = require("./mt-cameron.jpg");
+export const MT_COLUMBIA = require("./mt-columbia.jpg");
+export const MT_DEMOCRAT = require("./mt-democrat.jpg");
+export const MT_ELBERT = require("./mt-elbert.jpg");
+export const MT_EOLUS = require("./mt-eolus.jpg");
+export const MT_EVANS = require("./mt-evans.jpg");
+export const MT_HARVARD = require("./mt-harvard.jpg");
+export const MT_LINCOLN = require("./mt-lincoln.jpg");
+export const MT_LINDSEY = require("./mt-lindsey.jpg");
+export const MT_MASSIVE = require("./mt-massive.jpg");
+export const MT_OF_THE_HOLY_CROSS = require("./mt-of-the-holy-cross.jpg");
+export const MT_OXFORD = require("./mt-oxford.jpg");
+export const MT_PRINCETON = require("./mt-princeton.jpg");
+export const MT_SHAVANO = require("./mt-shavano.jpg");
+export const MT_SHERMAN = require("./mt-sherman.jpg");
+export const MT_SNEFFELS = require("./mt-sneffels.jpg");
+export const MT_WILSON = require("./mt-wilson.jpg");
+export const MT_YALE = require("./mt-yale.jpg");
+export const NORTH_EOLUS = require("./north-eolus.jpg");
+export const NORTH_MAROON_PEAK = require("./north-maroon-peak.jpg");
+export const PIKES_PEAK = require("./pikes-peak.jpg");
+export const PYRAMID_PEAK = require("./pyramid-peak.jpg");
+export const QUANDARY_PEAK = require("./quandary-peak.jpg");
+export const REDCLOUD_PEAK = require("./redcloud-peak.jpg");
+export const SAN_LUIS_PEAK = require("./san-luis-peak.jpg");
+export const SNOWMASS_MOUNTAIN = require("./snowmass-mountain.jpg");
+export const SUNLIGHT_PEAK = require("./sunlight-peak.jpg");
+export const SUNSHINE_PEAK = require("./sunshine-peak.jpg");
+export const TABEGUACHE_PEAK = require("./tabeguache-peak.jpg");
+export const TORREYS_PEAK = require("./torreys-peak.jpg");
+export const UNCOMPAHGRE_PEAK = require("./uncompahgre-peak.jpg");
+export const WETTERHORN_PEAK = require("./wetterhorn-peak.jpg");
+export const WILSON_PEAK = require("./wilson-peak.jpg");
+export const WINDOM_PEAK = require("./windom-peak.jpg");
 
 export const featurePhotos = [
-  {
-    name: "Blanca Peak",
-    photo: BLANCA_PEAK,
-  },
-  {
-    name: "Capitol Peak",
-    photo: CAPITOL_PEAK,
-  },
-  {
-    name: "Castle Peak",
-    photo: CASTLE_PEAK,
-  },
-  {
-    name: "Challenger Point",
-    photo: CHALLENGER_POINT,
-  },
-  {
-    name: "Conundrum Peak",
-    photo: CONUNDRUM_PEAK,
-  },
-  {
-    name: "Crestone Needle",
-    photo: CRESTONE_NEEDLE,
-  },
-  {
-    name: "Crestone Peak",
-    photo: CRESTONE_PEAK,
-  },
-  {
-    name: "El Diente Peak",
-    photo: EL_DIENTE_PEAK,
-  },
-  {
-    name: "Grays Peak",
-    photo: GRAYS_PEAK,
-  },
-  {
-    name: "Humboldt Peak",
-    photo: HUMBOLT_PEAK,
-  },
-  {
-    name: "Kit Carson Peak",
-    photo: KIT_CARSON_PEAK,
-  },
-  {
-    name: "La Plata Peak",
-    photo: LA_PLATA_PEAK,
-  },
-  {
-    name: "Longs Peak",
-    photo: LONGS_PEAK,
-  },
-  {
-    name: "Maroon Peak",
-    photo: MAROON_PEAK,
-  },
-  {
-    name: "Missouri Mountain",
-    photo: MISSOURI_MOUNTAIN,
-  },
-  {
-    name: "Mt. Antero",
-    photo: MT_ANTERO,
-  },
-  {
-    name: "Mt. Belford",
-    photo: MT_BELFORD,
-  },
-  {
-    name: "Mt. Bierstadt",
-    photo: MT_BIERSTADT,
-  },
+  { name: "Blanca Peak", photo: BLANCA_PEAK },
+  { name: "Capitol Peak", photo: CAPITOL_PEAK },
+  { name: "Castle Peak", photo: CASTLE_PEAK },
+  { name: "Challenger Point", photo: CHALLENGER_POINT },
+  { name: "Conundrum Peak", photo: CONUNDRUM_PEAK },
+  { name: "Crestone Needle", photo: CRESTONE_NEEDLE },
+  { name: "Crestone Peak", photo: CRESTONE_PEAK },
+  { name: "Culebra Peak", photo: CULEBRA_PEAK },
+  { name: "El Diente Peak", photo: EL_DIENTE_PEAK },
+  { name: "Ellingwood Point", photo: ELLINGWOOD_POINT },
+  { name: "Grays Peak", photo: GRAYS_PEAK },
+  { name: "Handies Peak", photo: HANDIES_PEAK },
+  { name: "Humboldt Peak", photo: HUMBOLDT_PEAK },
+  { name: "Huron Peak", photo: HURON_PEAK },
+  { name: "Kit Carson Peak", photo: KIT_CARSON_PEAK },
+  { name: "La Plata Peak", photo: LA_PLATA_PEAK },
+  { name: "Little Bear Peak", photo: LITTLE_BEAR_PEAK },
+  { name: "Longs Peak", photo: LONGS_PEAK },
+  { name: "Maroon Peak", photo: MAROON_PEAK },
+  { name: "Missouri Mountain", photo: MISSOURI_MOUNTAIN },
+  { name: "Mt. Antero", photo: MT_ANTERO },
+  { name: "Mt. Belford", photo: MT_BELFORD },
+  { name: "Mt. Bierstadt", photo: MT_BIERSTADT },
+  { name: "Mt. Bross", photo: MT_BROSS },
+  { name: "Mt. Cameron", photo: MT_CAMERON },
+  { name: "Mt. Columbia", photo: MT_COLUMBIA },
+  { name: "Mt. Democrat", photo: MT_DEMOCRAT },
+  { name: "Mt. Elbert", photo: MT_ELBERT },
+  { name: "Mt. Eolus", photo: MT_EOLUS },
+  { name: "Mt. Evans", photo: MT_EVANS },
+  { name: "Mt. Harvard", photo: MT_HARVARD },
+  { name: "Mt. Lincoln", photo: MT_LINCOLN },
+  { name: "Mt. Lindsey", photo: MT_LINDSEY },
+  { name: "Mt. Massive", photo: MT_MASSIVE },
+  { name: "Mt. of the Holy Cross", photo: MT_OF_THE_HOLY_CROSS },
+  { name: "Mt. Oxford", photo: MT_OXFORD },
+  { name: "Mt. Princeton", photo: MT_PRINCETON },
+  { name: "Mt. Shavano", photo: MT_SHAVANO },
+  { name: "Mt. Sherman", photo: MT_SHERMAN },
+  { name: "Mt. Sneffels", photo: MT_SNEFFELS },
+  { name: "Mt. Wilson", photo: MT_WILSON },
+  { name: "Mt. Yale", photo: MT_YALE },
+  { name: "North Eolus", photo: NORTH_EOLUS },
+  { name: "North Maroon Peak", photo: NORTH_MAROON_PEAK },
+  { name: "Pikes Peak", photo: PIKES_PEAK },
+  { name: "Pyramid Peak", photo: PYRAMID_PEAK },
+  { name: "Quandary Peak", photo: QUANDARY_PEAK },
+  { name: "Redcloud Peak", photo: REDCLOUD_PEAK },
+  { name: "San Luis Peak", photo: SAN_LUIS_PEAK },
+  { name: "Snowmass Mountain", photo: SNOWMASS_MOUNTAIN },
+  { name: "Sunlight Peak", photo: SUNLIGHT_PEAK },
+  { name: "Sunshine Peak", photo: SUNSHINE_PEAK },
+  { name: "Tabeguache Peak", photo: TABEGUACHE_PEAK },
+  { name: "Torreys Peak", photo: TORREYS_PEAK },
+  { name: "Uncompahgre Peak", photo: UNCOMPAHGRE_PEAK },
+  { name: "Wetterhorn Peak", photo: WETTERHORN_PEAK },
+  { name: "Wilson Peak", photo: WILSON_PEAK },
+  { name: "Windom Peak", photo: WINDOM_PEAK },
 ];

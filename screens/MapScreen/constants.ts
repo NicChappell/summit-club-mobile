cc2ac35257e93ab94bc81145f46d1ff85428f436
NicChappell <nwc@nicchappell.com>
2021-03-08 07:37:46 -0700

    adjust init map config

--- screens/MapScreen/constants.ts
+++ screens/MapScreen/constants.ts
@@ -16,19 +16,19 @@ export const featureFilters: IFeatureFilters = {
 // The initial map boundaries to query against
 export const initialMapBoundaries: IMapBoundaries = {
   northEast: {
-    latitude: 41.003906 + 1,
-    longitude: -102.042974 + 1,
+    latitude: 41.003906 + 1, // northeast latitude coordinate +1°
+    longitude: -102.042974 + 1, // northeast longitude coordinate +1°
   },
   southWest: {
-    latitude: 36.994786 - 1,
-    longitude: -109.058934 - 1,
+    latitude: 36.994786 - 1, // southwest latitude coordinate -1°
+    longitude: -109.058934 - 1, // southwest longitude coordinate -1°
   },
 };
 
 // The initial region to be displayed by the map
 export const initialRegion: Region = {
-  latitude: 39.113014,
-  longitude: -105.358887,
-  latitudeDelta: 5,
-  longitudeDelta: 5,
+  latitude: 39.331557, // center latitude coordinate
+  longitude: -105.5704935, // center longitude coordinate
+  latitudeDelta: 7.5,
+  longitudeDelta: 7.5,
 };

9bbe327fa32f3864a45a12f54acf2f90b45a72bf
NicChappell <nwc@nicchappell.com>
2021-03-24 07:29:24 -0600

    fix imports

--- navigation/navigators/HomeStack/index.tsx
+++ navigation/navigators/HomeStack/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { StackNavigatorHeader } from "../../../common/components";
+import { StackNavigatorHeader } from "../../../common/navigation";
 import {
   FeatureScreen,
   HomeScreen,
